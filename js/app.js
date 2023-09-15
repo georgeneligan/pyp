4c2286a60c20ff37316fa33c730f54e61999e954
georgeneligan <108015805+georgeneligan@users.noreply.github.com>
2023-09-15 17:11:32 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,12 +6,28 @@ let succsesfulyPage = document.querySelector(".ssecssesfuly-wrapper");
 let makeMoreBtn = document.querySelector(".make-more-btn");
 let countUsd = document.querySelector(".count-usd");
 let sendingUser = document.querySelector(".sending-user");
+let balanceBalance = document.querySelector(".balance-balance");
+let balanceCount = 478152000;
+let count = 0;
 
 transferBtn.addEventListener("click", () => {
-  headerPage.classList.add("hidden");
-  succsesfulyPage.classList.remove("hidden");
-  countUsd.innerHTML = `${inputUsd.value} $`;
-  sendingUser.innerHTML = `@${inputUser.value}`;
+  let numberValue = 0;
+  numberValue = inputUsd.value;
+  if (
+    balanceCount < numberValue ||
+    inputUsd.value === "" ||
+    inputUser.value === ""
+  ) {
+    console.log("error");
+  } else {
+    count = balanceCount - parseInt(numberValue);
+    balanceCount = balanceCount - parseInt(numberValue);
+    balanceBalance.innerHTML = `${count.toLocaleString()} $`;
+    headerPage.classList.add("hidden");
+    succsesfulyPage.classList.remove("hidden");
+    countUsd.innerHTML = `${inputUsd.value} $`;
+    sendingUser.innerHTML = `@${inputUser.value}`;
+  }
 });
 
 makeMoreBtn.addEventListener("click", () => {
